8ae74337f4aeaa18e1fbc730ab57b3b97aa9d9cf
LiziCode <1210678078@qq.com>
2023-05-02 22:40:19 +0800

    基于ListTile创建一个任务列表项

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,59 +1,198 @@
-// 导入 Flutter 核心库
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-// 导入自定义的 DetailPage 类
-import 'detail_page.dart';
+class Task {
+  final String name;
+  final DateTime deadline;
+  bool isCompleted;
 
-// 应用程序入口
-void main() {
-  // 运行应用程序
-  runApp(MyApp());
+  Task({required this.name, required this.deadline, this.isCompleted = false});
 }
 
-// 应用程序的根组件
-class MyApp extends StatelessWidget {
+class TaskListItem extends StatelessWidget {
+  final Task task;
+  final VoidCallback onTap;
+
+  TaskListItem({required this.task, required this.onTap});
+
   @override
   Widget build(BuildContext context) {
-    // 创建 MaterialApp 组件
-    return MaterialApp(
-      // 设置应用程序的标题
-      title: 'MyApp',
-      // 设置应用程序的初始路由
-      initialRoute: '/',
-      // 配置应用程序的路由表
-      routes: {
-        // 首页路由
-        '/': (context) => HomePage(),
-        // 详情页路由
-        '/detail': (context) => DetailPage(),
-      },
+    return ListTile(
+      leading: Checkbox(
+        value: task.isCompleted,
+        onChanged: (bool? value) {
+          onTap();
+        },
+      ),
+      title: Text(task.name),
+      subtitle: Text(
+        DateFormat('yyyy-MM-dd').format(task.deadline),
+        style: TextStyle(color: Colors.grey),
+      ),
     );
   }
 }
 
-// 首页组件
-class HomePage extends StatelessWidget {
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  List<Task> _tasks = [
+    Task(name: '完成Flutter作业', deadline: DateTime(2023, 5, 6)),
+    Task(name: '提交论文', deadline: DateTime(2023, 5, 9)),
+    Task(name: '购买礼物', deadline: DateTime(2023, 5, 10)),
+  ];
+
+  void _addTask(String name, DateTime deadline) {
+    setState(() {
+      _tasks.add(Task(name: name, deadline: deadline));
+    });
+  }
+
+  void _editTask(int index, String name, DateTime deadline) {
+    setState(() {
+      _tasks[index] = Task(name: name, deadline: deadline);
+    });
+  }
+
+  void _deleteTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+    });
+  }
+
+  void _toggleTaskCompletion(int index) {
+    setState(() {
+      _tasks[index].isCompleted = !_tasks[index].isCompleted;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // 创建 Scaffold 组件
     return Scaffold(
-      // 设置 Scaffold 组件的 AppBar
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('待办清单'),
       ),
-      // 设置 Scaffold 组件的主体内容
-      body: Center(
-        // 创建一个 ElevatedButton 组件
-        child: ElevatedButton(
-          // 设置按钮的文本
-          child: Text('Go to detail page'),
-          // 设置按钮的点击事件
-          onPressed: () {
-            // 跳转到详情页
-            Navigator.pushNamed(context, '/detail');
-          },
+      body: ListView.builder(
+        itemCount: _tasks.length,
+        itemBuilder: (context, index) {
+          return TaskListItem(
+            task: _tasks[index],
+            onTap: () => _toggleTaskCompletion(index),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddTaskScreen(
+                onSave: (name, deadline) => _addTask(name, deadline),
+              ),
+            ),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class AddTaskScreen extends StatefulWidget {
+  final Function(String, DateTime) onSave;
+
+  AddTaskScreen({required this.onSave});
+
+  @override
+  _AddTaskScreenState createState() => _AddTaskScreenState();
+}
+
+class _AddTaskScreenState extends State<AddTaskScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  DateTime _deadline = DateTime.now();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('添加任务'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: '任务名称',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '请输入任务名称';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              Text('任务截止日期'),
+              SizedBox(height: 8.0),
+              InkWell(
+                onTap: () async {
+                  final DateTime? picked = await showDatePicker(
+                    context: context,
+                    initialDate: _deadline,
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime(2100),
+                  );
+                  if (picked != null && picked != _deadline) {
+                    setState(() {
+                      _deadline = picked;
+                    });
+                  }
+                },
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.calendar_today),
+                    SizedBox(width: 8.0),
+                    Text(DateFormat('yyyy-MM-dd').format(_deadline)),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    widget.onSave(_nameController.text, _deadline);
+                    Navigator.pop(context);
+                  }
+                },
+                child: Text('保存'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    title: '待办清单',
+    home: TodoList(),
+  ));
 }
